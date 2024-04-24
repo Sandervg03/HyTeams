35394cc2081f73457ef05489d9c49836fe392baa
Sandervg03 <sander.van.goch2003@gmail.com>
2024-04-25 00:25:20 +0200

    Adds register page

--- frontend/HyTeams/src/app/components/register/register.component.ts
+++ frontend/HyTeams/src/app/components/register/register.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../service/user/user.service';
+import { User } from '../../models/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  registerUser() {
+    console.log('Registering user')
+    try {
+      this.userService.registerUser(new User('username', 'email'))
+      .subscribe((data) => { data instanceof User ? this.router.navigate(['/login']) : console.log('Error') });
+    } catch (error: any) {
+      const element: HTMLElement = document.getElementById('errorHandling') as HTMLElement;
+      element.innerHTML = error.message;
+    }
+  }
 
 }
