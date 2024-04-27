2f87d1423ac3d5a30ac5d34a3ca2860ec852686b
Sandervg03 <sander.van.goch2003@gmail.com>
2024-04-27 16:29:55 +0200

    #9 adds frontend login component

--- frontend/HyTeams/src/app/components/login/login.component.ts
+++ frontend/HyTeams/src/app/components/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../service/user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  error: string | null = null;
+
+  constructor(private service: UserService, private router: Router) { };
+
+  ngOnInit(): void {
+  }
+
+  loginUser() {
+    try {
+      const emailInput: HTMLInputElement = document.getElementById('email') as HTMLInputElement;
+      const passwordInput: HTMLInputElement = document.getElementById('password') as HTMLInputElement;
+      this.service.loginUser(emailInput.value, passwordInput.value)
+        .subscribe((response: boolean) => { 
+          this.router.navigate(['/home']); }, 
+        (error: any) => { 
+          this.error = error.message; 
+        });
+    } catch (error: any) {
+      this.error = error.message;
+    }
+  }
 
 }
--- frontend/HyTeams/src/app/service/user/user.service.ts
+++ frontend/HyTeams/src/app/service/user/user.service.ts
@@ -1,30 +1,33 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, retry } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService implements OnInit {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {}
-
-  public isLoggedIn(): Observable<boolean> {
-    return this.http
-     .get<boolean>('http://localhost:4001/isLoggedIn')
-     .pipe(retry(3));
-  }
+  ngOnInit(): void { }
 
   public registerUser(user: any): Observable<any> {
     return this.http
       .post<any>('http://localhost:4001/registerUser', user)
-      .pipe(retry(3));
   }
 
   public activateUser(email: string, code: string, password: string, confirmPassword: string): Observable<any> {
     return this.http
-      .post<any>('http://localhost:4001/activateUser', {_email: email, _code: code, _password: password, _confirmPassword: confirmPassword})
-      .pipe(retry(3));
+      .post<any>('http://localhost:4001/activateUser', { _email: email, _code: code, _password: password, _confirmPassword: confirmPassword })
   }
+
+  public isLoggedIn(): Observable<boolean> {
+    return this.http
+      .get<boolean>('http://localhost:4001/isLoggedIn')
+  }
+
+  public loginUser(email: string, password: string): Observable<boolean> {
+    return this.http
+      .post<boolean>('http://localhost:4001/loginUser', { _email: email, _password: password })
+  }
+
 }
